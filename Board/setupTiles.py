41cecc08f782517489740948ed9fc04fc06ab4cb
JMaxtos <jmmso4@iscte-iul.pt>
2025-07-09 18:19:11 +0100

    Game Functional

--- Board/setupTiles.py
+++ Board/setupTiles.py
@@ -1,13 +1,21 @@
-from constants import TILESIZE
-import os
 import pygame
-tile_numbers =[]
-for num in range(1,9):
-    tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons",f"Tile{num}.png")),(TILESIZE,TILESIZE)))
+import os
+from constants import TILESIZE
 
-tile_empty = tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons","TileEmpty.png")),(TILESIZE,TILESIZE)))
-tile_exploded = tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons","TileExploded.png")),(TILESIZE,TILESIZE)))
-tile_flag = tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons","TileFlag.png")),(TILESIZE,TILESIZE)))
-tile_mine = tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons","TileMine.png")),(TILESIZE,TILESIZE)))
-tile_notMine = tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons","TileNotMine.png")),(TILESIZE,TILESIZE)))
-tile_Unknown = tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Icons","TileUnknown.png")),(TILESIZE,TILESIZE)))
+tile_numbers = []
+
+# Carrega as tiles numeradas (Tile1.png até Tile8.png)
+for num in range(1, 9):
+    image = pygame.transform.scale(
+        pygame.image.load(os.path.join("Icons", f"Tile{num}.png")),
+        (TILESIZE, TILESIZE)
+    )
+    tile_numbers.append(image)
+
+# Carrega os restantes tiles (corretamente atribuídos)
+tile_Empty     = pygame.transform.scale(pygame.image.load(os.path.join("Icons", "TileEmpty.png")),     (TILESIZE, TILESIZE))
+tile_Exploded  = pygame.transform.scale(pygame.image.load(os.path.join("Icons", "TileExploded.png")),  (TILESIZE, TILESIZE))
+tile_Flag      = pygame.transform.scale(pygame.image.load(os.path.join("Icons", "TileFlag.png")),      (TILESIZE, TILESIZE))
+tile_Mine      = pygame.transform.scale(pygame.image.load(os.path.join("Icons", "TileMine.png")),      (TILESIZE, TILESIZE))
+tile_NotMine   = pygame.transform.scale(pygame.image.load(os.path.join("Icons", "TileNotMine.png")),   (TILESIZE, TILESIZE))
+tile_Unknown   = pygame.transform.scale(pygame.image.load(os.path.join("Icons", "TileUnknown.png")),   (TILESIZE, TILESIZE))
--- Board/tile.py
+++ Board/tile.py
@@ -5,21 +5,21 @@ class Tile:
     # "M" -> mine
     # "C" -> clue
     # "/" ->empty
-    def __init__(self,x,y,image,type,revealed='False',flagged='False'):
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
+    
+    def __init__(self, x, y, image, type, revealed=False, flagged=False):
+        self.x, self.y = x * TILESIZE, y * TILESIZE
         self.image = image
         self.type = type
         self.revealed = revealed
         self.flagged = flagged
-    
+
     def draw(self, boardGame):
         if not self.flagged and self.revealed:
-            boardGame.blit(self.image,(self.x,self.y))  
+            boardGame.blit(self.image, (self.x, self.y))
         elif self.flagged and not self.revealed:
-            boardGame.blit(tile_flag,(self.x,self.y))
-        else: 
-            boardGame.blit(tile_Unknown,(self.x,self.y))
+            boardGame.blit(tile_Flag, (self.x, self.y))
+        elif not self.revealed:
+            boardGame.blit(tile_Unknown, (self.x, self.y))
 
     def __repr__(self):
         return self.type
